e2aa82b9dc525ba611df70ee3bd1433407c53fc9
Rafael Cruz <rafael.cruz@prontmed.com>
2022-04-02 09:17:39 -0300

    setup

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Div - Divisão fácil</title>
+        <meta name="description" content="Home" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Home</h1>
+      </main>
+    </>
+  )
+}
