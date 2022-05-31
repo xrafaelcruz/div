e52d6a8f7b804e5df909d736d2268e5ac29947b5
Rafael Cruz <rafael.cruz@prontmed.com>
2022-05-31 15:40:02 -0300

    remove .env and migrations

--- prisma/migrations/20220420012809_changing_relation/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Group" DROP CONSTRAINT "Group_ownerUserEmail_fkey";
-
--- AddForeignKey
-ALTER TABLE "Group" ADD CONSTRAINT "Group_ownerUserEmail_fkey" FOREIGN KEY ("ownerUserEmail") REFERENCES "User"("email") ON DELETE RESTRICT ON UPDATE CASCADE;
